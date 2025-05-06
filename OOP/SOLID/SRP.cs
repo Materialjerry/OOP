9300d1929f10cf6abe758c1bb1efa70f4338a6b2
Materialjerry <bruno.andabaka@gmail.com>
2025-05-06 19:16:04 +0200

    Resolved Solid

---
+++ OOP/SOLID/SRP.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.SOLID
+{
+    class SRP
+    {
+        public class BadReport
+        {
+            public void GenerateReport()
+            {
+                Console.WriteLine("Report generated.");
+            }
+            public void SaveToPdf()
+            {
+                Console.WriteLine("Report saved to PDF.");
+            }
+            public void SendEmail()
+            {
+                Console.WriteLine("Report sent via email.");
+            }
+        }
+
+        public class Report
+        {
+            public void GenerateReport()
+            {
+                Console.WriteLine("Report generated.");
+            }
+        }
+
+        public class Pdf
+        {
+            public void SaveToPdf()
+            {
+                Console.WriteLine("Report saved to PDF.");
+            }
+        }
+
+        public class EmailService
+        {
+            public void SendEmail()
+            {
+                Console.WriteLine("Report sent via email.");
+            }
+        }
+    }
+}
+
